29b866cfd7ce4f5d5588e4cf86836c998f132dde
priority <1832052104@qq.com>
2022-10-08 17:01:10 +0800

    fix: computed effect

--- packages/reactivity/__test__/computed.spec.ts
+++ packages/reactivity/__test__/computed.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi } from 'vitest'
-import { computed, reactive } from '../src'
+import { computed, effect, reactive } from '../src'
 
 describe('reactivity/computed', () => {
   it('basic computed', () => {
@@ -15,5 +15,20 @@ describe('reactivity/computed', () => {
 
     obj.foo++
     expect(sumRes.value).toBe(4)
+  })
+  it('effect computed', () => {
+    const data = { foo: 1, bar: 2 }
+    const obj = reactive(data)
+    const fn = vi.fn(() => obj.foo + obj.bar)
+    const sumRes = computed(fn)
+    let count = 0
+    effect(() => {
+      count = sumRes.value
+    })
+    expect(count).toBe(3)
+
+    obj.foo++
+
+    expect(count).toBe(4)
   })
 })
--- packages/reactivity/src/computed.ts
+++ packages/reactivity/src/computed.ts
@@ -1,4 +1,4 @@
-import { effect } from './effect'
+import { effect, track, trigger } from './effect'
 
 export type ComputedGetter<T> = (...args: any[]) => T
 export function computed<T>(
@@ -6,24 +6,27 @@ export function computed<T>(
 ) {
   let dirty = true
   let value
+  let resObj
   const effectFn = effect(getter, {
     lazy: true,
     scheduler() {
       dirty = true
+      trigger(resObj, 'value')
     },
 
   })
 
-  const obj = {
+  resObj = {
     get value() {
       if (dirty) {
         value = effectFn()
         dirty = false
       }
+      track(resObj, 'value')
 
       return value
     },
   }
 
-  return obj
+  return resObj
 }
